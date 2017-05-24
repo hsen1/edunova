203737aedb241d5d21e30cacacc5d1abcff514df
Hrvoje <hrvojesen@gmail.com>
2017-05-24 15:07:23 +0200

    Add files via upload

--- projekt/webshop.sql
+++ projekt/webshop.sql
@@ -5,13 +5,14 @@ use webshop;
 create table proizvod(
 proizvod_ID			int (6) not null primary key auto_increment,
 proizvod_naziv		varchar (50) not null,
-proizvod_opis		text not null,
+proizvod_opis		varchar (1000) not null,
 proizvod_cijena		decimal (18,2) not null,
+proizvod_dobSifra	varchar (30),
 proizvod_brand		varchar (50),
 proizvod_garancija	int (2) default 0,
 proizvod_kolicina	int (4) not null default 0,
 dobavljac_FK		int (6) not null,
-kategorija_FK		int (6) not null
+kategorija_fk		int (6) not null
 );
 
 create table kupac(
@@ -33,6 +34,7 @@ narudzba_broj		varchar (20) not null,
 narudzba_datum		datetime not null,
 narudzba_status		varchar (15) not null,
 narudzba_napomena	varchar (200),
+dostava_FK			int (6) not null,
 kupac_FK			int (6) not null
 );
 
@@ -50,16 +52,15 @@ dobavljac_postanskiBroj	int (5)
 create table kosarica(
 proizvod_FK			int (6) not null,
 narudzba_FK			int (6) not null,
-dostava_FK			int (6) not null,
 kosarica_kolicina	int (4) not null,
 kosarica_cijena		decimal (18,2) not null,
-primary key (proizvod_FK, narudzba_FK, dostava_FK)
+primary key (proizvod_FK, narudzba_FK)
 );
 
 create table kategorija(
 kategorija_ID		int (6) primary key auto_increment,
 kategorija_naziv	varchar (50) not null,
-kategorija_slika	blob,
+kategorija_slika	varchar (100),
 kategorija_caption	varchar (50)
 );
 
@@ -73,7 +74,8 @@ alter table proizvod add foreign key (dobavljac_FK) references dobavljac(dobavlj
 alter table proizvod add foreign key (kategorija_FK) references kategorija(kategorija_ID);
 
 alter table narudzba add foreign key (kupac_FK) references kupac(kupac_ID);
+alter table narudzba add foreign key (dostava_FK) references dostava(dostava_ID);
 
 alter table kosarica add foreign key (proizvod_FK) references proizvod(proizvod_ID);
 alter table kosarica add foreign key (narudzba_FK) references narudzba(narudzba_ID);
-alter table kosarica add foreign key (dostava_FK) references dostava(dostava_ID);
+
