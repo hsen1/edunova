e6c270986a057b49c042305f60965fe374737241
Hrvoje <hrvojesen@gmail.com>
2017-05-31 15:07:31 +0200

    Add files via upload

---
+++ zadaci/dz1/head2.php
@@ -0,0 +1,6 @@
+<link href="http://fonts.googleapis.com/css?family=Chewy" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Terminal+Dosis+Light" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Maven+Pro" rel="stylesheet" type="text/css">
+
+<link href="styles/custom.css" rel="stylesheet" type="text/css" />
