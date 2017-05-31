5796787a86051937b2b47187964dd2010c76305c
Hrvoje <hrvojesen@gmail.com>
2017-05-31 15:03:30 +0200

    Delete head2.php

--- dz1/head2.php
+++
@@ -1,6 +0,0 @@
-<link href="http://fonts.googleapis.com/css?family=Chewy" rel="stylesheet" type="text/css">
-<link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css">
-<link href="http://fonts.googleapis.com/css?family=Terminal+Dosis+Light" rel="stylesheet" type="text/css">
-<link href="http://fonts.googleapis.com/css?family=Maven+Pro" rel="stylesheet" type="text/css">
-
-<link href="styles/custom.css" rel="stylesheet" type="text/css" />
